3bacabb1cd50948750433e8faf1ccc47461b833c
Honey Jill Hubahib <jillhubahib@gmail.com>
2020-08-26 10:41:07 +0800

    Add support switching to dark theme with alpine

--- twitter_url_extractor_client/app/javascript/packs/application.js
+++ twitter_url_extractor_client/app/javascript/packs/application.js
@@ -6,15 +6,18 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
+require("alpinejs")
+
+import "../stylesheets/application.scss"
+import "controllers"
+import { data } from "../utils/helpers"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
 
-require("alpinejs")
-import "../stylesheets/application.scss"
-import "controllers"
+window.data = data // alpine - to support dark theme
